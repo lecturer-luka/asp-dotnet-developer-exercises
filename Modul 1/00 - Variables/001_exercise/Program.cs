c97ec4c85887c8e2c908ca933cdecb7b7be2250a
Luka Nekić <luka.nekic@predavaci.algebra.hr>
2022-12-06 12:29:02 +0100

    Added the first two exercises to the chapter "Variables"

---
+++ Modul 1/00 - Variables/001_exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * With the help of basic variables and the Console.ReadLine() command save two values, then assign variable "a" the entered value of variable "b" and assign variable "b" the entered value of variable "a"
+ */
+
+// Here we have basic logic to insert data
+// (in this example input validation is not required, the user is expected to enter an integer values)
+Console.Write("Enter a value for variable \"a\": ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter a value for variable \"b\": ");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("\n\nBefore:\na = {0}\nb = {1}", a, b);
+
+// Insert your code logic here to replace values between variables
+
+Console.WriteLine("\nAfter:\na = {0}\nb = {1}", a, b);
+
+
+// This is only here to stop the program before exiting
+Console.ReadKey();
---
+++ Modul 1/00 - Variables/002_exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Using basic variables and arithmetic operators write a simple program that takes distance and time as input
+ * and displays speed km/h and mph.
+ */
+
+float distance, hour, minutes, seconds;
+float kilometres_per_hour = 0.0F, miles_per_hour = 0.0F;
+
+// Here we have basic logic to insert data
+// (in this example input validation is not required, the user is expected to enter an integer or float value type)
+Console.Write("Enter the distance (meters): ");
+distance = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter the time (hour): ");
+hour = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter the time (minutes): ");
+minutes = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter the time (seconds): ");
+seconds = Convert.ToSingle(Console.ReadLine());
+
+
+// Insert your code logic here to calculate speed in kilometers per hour and miles per hour
+
+
+
+// Here we have data output
+Console.WriteLine("Your speed in \"km/h\" is {0}", kilometres_per_hour);
+Console.WriteLine("Your speed in \"mph\" is {0}", miles_per_hour);
+
+
+// This is only here to stop the program before exiting
+Console.ReadKey();
