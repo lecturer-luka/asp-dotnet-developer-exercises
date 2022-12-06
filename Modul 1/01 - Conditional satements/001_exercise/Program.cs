2a74d43b23596f58d1ad14fb01ebd6479fc156fa
Luka Nekić <luka.nekic@predavaci.algebra.hr>
2022-12-06 10:37:55 +0100

    Minor changes in the directory structure. Added the first exercises

---
+++ Modul 1/01 - Conditional satements/001_exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Using If statement write a simple program to find eligibility for admission to professional studies
+ */
+
+int physics, chemistry, mathematics;
+
+// The rules for your conditions can be found in the next sentences
+Console.Write("The candidate can enroll in the study program only if he meets the following criteria: \n");
+Console.Write("Points in Mathematics >=65\n");
+Console.Write("and points in Physics >=55\n");
+Console.Write("and points in Chemistry >=50\n");
+Console.Write("and total for all three subjects >=180\n");
+Console.Write("or total in Mathematics and Physics >=140\n");
+Console.Write("-------------------------------------\n");
+
+// Here we have basic logic to insert data
+// (in this example input validation is not required, the user is expected to enter an integer values)
+Console.Write("Enter the points obtained in Physics (0-100):");
+physics = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the points obtained in Chemistry (0-100):");
+chemistry = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the points obtained in Mathematics (0-100):");
+mathematics = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Total number of points in Mathematics, Physics and Chemistry: {0}\n", mathematics + physics + chemistry);
+Console.Write("Total number of points in Mathematics and Physics: {0}\n", mathematics + physics);
+Console.WriteLine();
+
+
+// Insert your code logic below using combination of if, else if and else statements
+
+
+
+// This is only here to stop the program before exiting
+Console.ReadKey();
---
+++ Modul 1/01 - Conditional satements/002_exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Using Switch statement write a simple menu-driven program to perform a calculation of two numbers.
+ */
+
+using System;
+
+int first_number, second_number, select_option;
+
+// Here we have basic logic to insert data
+// (in this example input validation is not required, the user is expected to enter an integer values)
+Console.Write("Unesite prvi broj: ");
+first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Unesite drugi broj: ");
+second_number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nSelect a calculation operation:\n");
+Console.Write("1-Addition.\n2-Deduction.\n3-Multiplication.\n4-Division.\n5-Exit the program.\n");
+Console.Write("\nSelect one option by number: ");
+select_option = Convert.ToInt32(Console.ReadLine());
+
+// Insert your code logic below using switch expression with case, don't forget about break and default keywords
+
+
+
+// This is only here to stop the program before exiting
+Console.ReadKey();
